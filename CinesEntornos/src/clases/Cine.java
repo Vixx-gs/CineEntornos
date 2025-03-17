f22b57050e265e8dcd0baeca34012e737c0a8455
Vixx-gs <victorgallegoserrate@gmail.com>
2025-03-17 11:49:24 +0100

    Constructores y getters generados

--- CinesEntornos/src/clases/Cine.java
+++ CinesEntornos/src/clases/Cine.java
@@ -5,4 +5,45 @@ public class Cine {
 	public int butacasOcupadas;
 	public String tituloPelicula;
 	public int precioEntrada;
+	public Cine() {
+		super();
+	}
+	public Cine(int aforo) {
+		super();
+		this.aforo = aforo;
+	}
+	public Cine(int aforo, String tituloPelicula, int precioEntrada) {
+		super();
+		this.aforo = aforo;
+		this.tituloPelicula = tituloPelicula;
+		this.precioEntrada = precioEntrada;
+		aforo = 100;
+		butacasOcupadas = 0;
+		tituloPelicula = "Sin determinar";
+		precioEntrada = 5;
+	}
+	public int getAforo() {
+		return aforo;
+	}
+	public void setAforo(int aforo) {
+		this.aforo = aforo;
+	}
+	public int getButacasOcupadas() {
+		return butacasOcupadas;
+	}
+	public void setButacasOcupadas(int butacasOcupadas) {
+		this.butacasOcupadas = butacasOcupadas;
+	}
+	public String getTituloPelicula() {
+		return tituloPelicula;
+	}
+	public void setTituloPelicula(String tituloPelicula) {
+		this.tituloPelicula = tituloPelicula;
+	}
+	public int getPrecioEntrada() {
+		return precioEntrada;
+	}
+	public void setPrecioEntrada(int precioEntrada) {
+		this.precioEntrada = precioEntrada;
+	}
 }
